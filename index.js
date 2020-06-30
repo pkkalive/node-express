fce7358dffe69b7c79801e98abb7cff911c47b0f
Purushotham Kumar <kalivepurushothamkumar@gmail.com>
2020-06-30 22:00:26 +0530

    Express Simple REST

--- index.js
+++ index.js
@@ -51,6 +51,15 @@ app.delete('/dishes/:dishId', (req, res, next) => {
     res.end('Deleting dish: ' + req.params.dishId);
 });
 
+app.use(express.static(__dirname + '/public'));
+app.use((req, res, next) => {
+    console.log(req.headers);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+
+});
+
 const server = http.createServer(app);
 
 server.listen(port, hostname, () => {
